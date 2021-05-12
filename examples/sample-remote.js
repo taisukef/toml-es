7ca8ba05b3715c6bd99d29e71116867479609a22
Taisuke Fukuno <fukuno@jig.jp>
2021-05-13 07:23:58 +0900

    split examples

--- examples/sample-remote.js
+++ examples/sample-remote.js
@@ -1,8 +1,8 @@
 //import { TOML } from "https://js.sabae.cc/TOML.js";
-import { TOML } from "https://taisukef.github.io/toml-js/es/TOML.js";
+import { TOML } from "https://taisukef.github.io/toml-es/TOML.js";
 
 const a = { name: "joe", age: 20 };
-const s = TOML.dump(a);
+const s = TOML.stringify(a);
 console.log(s);
 const a2 = TOML.parse(s);
 console.log(a2);
--- test/array.test.js
+++ test/array.test.js
@@ -1,41 +1,9 @@
 import * as t from "https://deno.land/std/testing/asserts.ts";
 import { TOML } from "../TOML.js";
-import { parseURL } from "https://code4sabae.github.io/js/parseURL.js";
-const { dirname } = parseURL(import.meta.url);
 
-//return (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/).test(value);
-
-Deno.test("simple", () => {
-  t.assertEquals(TOML.parse("a=123"), { a: 123 });
-});
-Deno.test("date", () => {
-  t.assertEquals(TOML.parse("date = 1979-05-27T07:32:00Z"), {
-    date: new Date("1979-05-27T07:32:00.000Z"),
-  });
-});
-Deno.test("data", async () => {
-  const s = await Deno.readTextFile(dirname + "example.toml");
-  const d = TOML.parse(s);
-  const chk = {
-    title: "TOMLExample",
-    owner: {
-      name: "TomPreston-Werner",
-      organization: "GitHub",
-      bio: "GitHubCofounder&CEO\nLikestatertotsandbeer.",
-      dob: new Date("1979-05-27T07:32:00.000Z"),
-    },
-    database: {
-      server: "192.168.1.1",
-      ports: [8001, 8001, 8002],
-      connection_max: 5000,
-      enabled: true,
-    },
-    servers: {
-      alpha: { ip: "10.0.0.1", dc: "eqdc10" },
-      beta: { ip: "10.0.0.2", dc: "eqdc10" },
-    },
-    clients: { data: [["gamma", "delta"], [1, 2]] },
-    hosts: ["alpha", "omega"],
-  };
-  t.assertEquals(d, chk);
+Deno.test("array", () => {
+  const a = [{ name: "joe", age: 20 }, { name: "kate", age: 21 }];
+  const s = TOML.stringify(a);
+  const a2 = TOML.parse(s);
+  t.assertEquals(a, a2);
 });
